f13a74c9b62703bdc365d043aa3be30ad2c8eaaa
Kimmy Leo <kenpusney@gmail.com>
2016-03-30 12:45:37 +0800

    remove ropto cpp file

--- include/ropto/ByteStream.hpp
+++ include/ropto/ByteStream.hpp
@@ -21,23 +21,44 @@ namespace ropto
         bytes_t bytes {};
         int index = 0;
     public:
-        byte_stream();
+        byte_stream() {};
         
         byte_stream(const byte_stream& stream) = delete;
         
-        void append(uint8_t byte);
+        void append(uint8_t byte) 
+        {
+            bytes.push_back(byte);
+        }
         
-        uint8_t fetch();
+        uint8_t fetch() 
+        {
+            return bytes[index++];
+        }
         
-        bytes_t& iterate();
+        bytes_t& iterate() 
+        {
+            return bytes;
+        }
         
-        auto begin() -> decltype(bytes.begin()) const;
+        auto begin() -> decltype(bytes.begin()) const
+        {
+            return std::begin(bytes);
+        }
         
-        auto end() -> decltype(bytes.end()) const;
+        auto end() -> decltype(bytes.end()) const
+        {
+            return std::end(bytes);
+        }
         
-        size_t size() const;
+        size_t size() const
+        {
+            return bytes.size();
+        }
         
-        bool fit(size_t size) const;
+        bool fit(size_t size) const
+        {
+            return index + size < this -> size();
+        }
         
         template<class T>
         bool fit() const
@@ -45,7 +66,6 @@ namespace ropto
             return fit(sizeof(T));
         }
     };
-    
 
 }
 
